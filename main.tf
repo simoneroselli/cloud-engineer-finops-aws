ebf4acb0369eaf28436d417e67b0807cffbb391d
Simone Roselli <zmo@germanomacbook.fritz.box>
2026-08-29 18:36:18 +0200

    feat: organize data files into subdirectory and initialize terraform configuration with gitignore

--- main.tf
+++ main.tf
@@ -13,15 +13,15 @@ resource "aws_s3_bucket" "athena_results" {
 resource "aws_s3_object" "cur_data" {
   bucket = aws_s3_bucket.finops_data.id
   key    = "cur/cur_data.csv"
-  source = "${path.module}/cur_data.csv"
-  etag   = filemd5("${path.module}/cur_data.csv")
+  source = "${path.module}/data/cur_data.csv"
+  etag   = filemd5("${path.module}/data/cur_data.csv")
 }
 
 resource "aws_s3_object" "mau_data" {
   bucket = aws_s3_bucket.finops_data.id
   key    = "mau/mau_data.csv"
-  source = "${path.module}/mau_data.csv"
-  etag   = filemd5("${path.module}/mau_data.csv")
+  source = "${path.module}/data/mau_data.csv"
+  etag   = filemd5("${path.module}/data/mau_data.csv")
 }
 
 # --- Athena Resources ---
