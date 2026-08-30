710721506b23c728c3a86c82f29f2f7c1ffaf2d9
Simone Roselli <zmo@germanomacbook.fritz.box>
2026-08-30 22:08:10 +0200

    feat: add Terraform configuration for S3 buckets, Lambda functions, and outputs for untagged spend reporting

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,15 +8,15 @@ resource "aws_s3_bucket" "finops_data" {
 resource "aws_s3_object" "cur_data" {
   bucket = aws_s3_bucket.finops_data.id
   key    = "cur/cur_data.csv"
-  source = "${path.module}/data/cur_data.csv"
-  etag   = filemd5("${path.module}/data/cur_data.csv")
+  source = "${path.module}/../data/cur_data.csv"
+  etag   = filemd5("${path.module}/../data/cur_data.csv")
 }
 
 resource "aws_s3_object" "mau_data" {
   bucket = aws_s3_bucket.finops_data.id
   key    = "mau/mau_data.csv"
-  source = "${path.module}/data/mau_data.csv"
-  etag   = filemd5("${path.module}/data/mau_data.csv")
+  source = "${path.module}/../data/mau_data.csv"
+  etag   = filemd5("${path.module}/../data/mau_data.csv")
 }
 
 # --- Lambda Functions: Untagged Spend ---
@@ -25,8 +25,8 @@ module "untagged_spend" {
   source = "./modules/athena_lambda_reporter"
 
   function_name = "finops_untagged_spend_reporter"
-  script_file_path = "${path.module}/bin/untagged_spend.py"
-  sql_file_path    = "${path.module}/athena/untagged_spend.sql"
+  script_file_path = "${path.module}/../bin/untagged_spend.py"
+  sql_file_path    = "${path.module}/../athena/untagged_spend.sql"
   handler          = "untagged_spend.lambda_handler"
 
   environment_variables = {
